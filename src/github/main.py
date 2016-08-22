87835b11b14bd424ac0ddd8a55b1dd3a2c4831de
dwadeflash <dwadeflash@users.noreply.github.com>
2016-08-23 01:03:04 +0800

    使用redis的list保存待扫描的用户，set保存已经扫描的用户

--- src/github/main.py
+++ src/github/main.py
@@ -8,6 +8,7 @@ import urllib.request
 from bs4 import BeautifulSoup
 import mysql.connector
 import time
+import redis
 
 def getMainInfo(userHome, headers):
     request = urllib.request.Request(userHome, headers = headers)
@@ -16,7 +17,10 @@ def getMainInfo(userHome, headers):
     soup = BeautifulSoup(plain_text, "html.parser")
     full_name = soup.find('div', {'class':'vcard-fullname'}).get_text()
     user_name = soup.find('div', {'class':'vcard-username'}).get_text()
-    location = soup.find('li', {'aria-label':'Home location'}).get_text()
+    locationTag = soup.find('li', {'aria-label':'Home location'})
+    location = ""
+    if locationTag:
+        location = locationTag.get_text()
     join_time = soup.find('local-time', {'class':'join-date'})['datetime']
     info = soup.find_all('strong', {'class':'vcard-stat-count d-block'})
     followers = info[0].get_text()
@@ -42,27 +46,34 @@ def getStarsAndForks(repositories, headers, user):
     stars = 0
     forks = 0
     for repo in repoList:
-        aTag = repo.find_all('a')
-        for e in aTag:
-            print(e.get_text())
+        aTag = repo.find('div', {'class':'repo-list-stats'}).find_all('a')
+        stars += int(aTag[0].get_text().strip('\n').strip().replace(',',''))
+        forks += int(aTag[1].get_text().strip('\n').strip().replace(',',''))
     user['stars'] = stars
     user['forks'] = forks
+    
+def getFollowers(followersUrl, headers):
+    request = urllib.request.Request(followersUrl, headers = headers)
+    source_code = urllib.request.urlopen(request).read()
+    plain_text = source_code.decode("utf-8")
+    soup = BeautifulSoup(plain_text, "html.parser")
+    followerList = soup.find_all('span', {'class':'css-truncate css-truncate-target'})
+    followers = []
+    for follower in followerList:
+        aTag = follower.find('a')
+        if aTag:
+            followers.append(aTag.get('href').replace('/',''))
+    return followers
 
-if __name__ == '__main__':
-    user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-    headers = { 'User-Agent' : user_agent }
-    userHome = "https://github.com/Vedenin"
-    repositories = userHome + "?tab=repositories"
-    user = getMainInfo(userHome, headers)
-    getStarsAndForks(repositories, headers, user)
+def saveUser(user):
     config = {
         'user': 'root',
-        'password': 'root',
+        'password': 'root', 
         'host': '127.0.0.1',
         'database': 'github-spider'
     }
-    add_user = ("INSERT INTO user (name, full_name, location, join_time, followers, starred, following) VALUES "
-                "(%(name)s, %(full_name)s, %(location)s, %(join_time)s, %(followers)s, %(starred)s, %(following)s)")
+    add_user = ("INSERT INTO user (name, full_name, location, join_time, followers, starred, following, stars, forks) VALUES "
+                "(%(name)s, %(full_name)s, %(location)s, %(join_time)s, %(followers)s, %(starred)s, %(following)s, %(stars)s, %(forks)s)")
     cnx = cur = None
     try:
         cnx = mysql.connector.connect(**config)
@@ -77,4 +88,35 @@ if __name__ == '__main__':
             cur.close()
         if cnx:
             cnx.close()
-        
+    
+def startSpider():
+    user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+    headers = { 'User-Agent' : user_agent }
+    redisConfig = {
+        'host': '192.168.1.104',
+        'port': 6379,
+        'password':123
+    }
+    r = redis.StrictRedis(**redisConfig)
+    r.lpush("githubspider-toScanUsers", "sanitar4eg")
+    name = r.lpop("githubspider-toScanUsers").decode()
+    while name != None:
+        print(name)
+        userHome = "https://github.com/" + name
+        repositories = userHome + "?tab=repositories"
+        followersUrl = userHome + "/followers"
+        try:
+            user = getMainInfo(userHome, headers)
+            getStarsAndForks(repositories, headers, user)
+            saveUser(user)
+            followers = getFollowers(followersUrl, headers)
+            for follower in followers:
+                if not r.sismember("githubspider-scannedUsers", name):
+                    r.rpush("githubspider-toScanUsers", follower)
+        except Exception:
+            print("error happend")
+        r.sadd("githubspider-scannedUsers", name)
+        name = r.lpop("githubspider-toScanUsers").decode()
+
+if __name__ == '__main__':
+    startSpider()
